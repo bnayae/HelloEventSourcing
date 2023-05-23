3be32bf9d0478c45153cabe3077fc3884c89512c
Bnaya Eshet <3382669+bnayae@users.noreply.github.com>
2023-05-23 13:07:47 +0300

    Add project files.

---
+++ EventsAbstractions/IHelloEvents.cs
@@ -0,0 +1,10 @@
+﻿namespace EventsAbstractions;
+
+
+/// <summary>
+/// Event's schema definition
+/// Return type of each method should be  <see cref="System.Threading.Tasks.ValueTask"/>
+/// </summary>
+public interface IHelloEvents
+{
+}
